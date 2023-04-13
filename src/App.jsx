c9d1317cf364d8379471f297e5630713748ecd7f
Sol Gutierrez <sdgselva@gmail.com>
2023-04-13 08:26:21 -0600

    Core files cretated

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Header from './components/header/header'
+import Nav from './components/nav/nav'
+import About from './components/about/about'
+import Contact from './components/contact/contact'
+import Experience from './components/experience/experience'
+import Portfolio from './components/portfolio/portfolio'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Contact />
+      <Experience />
+      <Portfolio />
+
+    </>
+  )
+}
+
+export default App
